ca65c170b9ec419ccab19340850d6f4d25b35955
ithute-stak <thekoetlisi@ithute.co.ls>
2026-09-07 20:14:53 +0200

    feat(web): register authentication and API services

--- apps/web/Program.cs
+++ apps/web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using gRisk.Web;
+using gRisk.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,5 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "http://localhost:8000/";
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<ApiClient>();
 
 await builder.Build().RunAsync();
