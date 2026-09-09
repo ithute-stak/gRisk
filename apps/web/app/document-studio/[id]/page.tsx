efb517771a07500b0d9bb3573d28b4ab3bbd944e
ithute-stak <thekoetlisi@ithute.co.ls>
2026-09-09 07:49:11 +0200

    Expose editable letterhead details in Studio

--- apps/web/app/document-studio/[id]/page.tsx
+++ apps/web/app/document-studio/[id]/page.tsx
@@ -5,8 +5,12 @@ import "../loanhub-editor.css";
 import "../official-letterhead.css";
 import { useParams } from "next/navigation";
 import LoanHubDocumentStudioEditor from "@/components/LoanHubDocumentStudioEditor";
+import StudioLetterheadCustomization from "@/components/StudioLetterheadCustomization";
 
 export default function DocumentStudioEditorPage() {
   const params = useParams<{ id: string }>();
-  return <LoanHubDocumentStudioEditor documentId={params.id} />;
+  return <>
+    <LoanHubDocumentStudioEditor documentId={params.id} />
+    <StudioLetterheadCustomization documentId={params.id} />
+  </>;
 }
